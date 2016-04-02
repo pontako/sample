d20617c28033c1a66cd5dfcde67e5fc1b36cc390
Your Name <you@example.com>
2016-04-02 16:26:27 +0900

    add sign_apk

---
+++ sign_apk.sh
@@ -0,0 +1,1 @@
+java -jar out/host/linux-x86/framework/signapk.jar build/target/product/security/testkey.x509.pem build/target/product/security/testkey.pk8 out/target/product/generic/obj/APPS/HelloAndroid_intermediates/package.apk.unsigned out/target/product/generic/obj/APPS/HelloAndroid_intermediates/package.apk.signed
